3503df8f7b1aa7c2d17436d6ab9cf202bdd68958
Victoria Brown <victoriabrown0710@gmail.com>
2016-10-06 15:28:33 -0500

    Upload ex10.py from LPTHW

---
+++ ex10.py
@@ -0,0 +1,23 @@
+# Simple program to see escape characters in use with Python language.
+# Programmer:           Victoria Brown
+# Date:                 October 2016
+
+# Variables with various escape characters
+# \t = tab
+# \n = newline
+# \\ = \
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Print the variables
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
