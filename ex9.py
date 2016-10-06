c349aee9530f6f9cd5382bb8853d3089734e5a73
Victoria Brown <victoriabrown0710@gmail.com>
2016-10-06 14:53:28 -0500

    Upload ex9.py from LPTHW

---
+++ ex9.py
@@ -0,0 +1,21 @@
+# Simple program to practice printing in Python language.
+# Programmer:           Victoria Brown
+# Date:                 October 2016
+
+# Here's some new strange stuff.
+
+# Variables:
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print the variables.
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# Print using 3 quotation marks
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
